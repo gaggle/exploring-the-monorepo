bf549703c3a701158afa8b24f3cffe7ec53c40a3
Jon Lauridsen <mail@jonlauridsen.com>
2021-06-27 00:00:33 +0200

    Delete non-web code

--- libs/analytics/src/index.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ApiAnalytics } from '.'
-
-describe('ApiAnalytics', () => {
-  it('exposes #foundAllUsers', () => {
-    const analytics = new ApiAnalytics()
-    expect(analytics).toHaveProperty('foundAllUsers')
-  })
-})
